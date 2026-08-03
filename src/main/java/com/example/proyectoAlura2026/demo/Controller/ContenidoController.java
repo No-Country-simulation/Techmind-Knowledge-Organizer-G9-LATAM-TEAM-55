c81c244a9f38c5c04e6bdf518059ec27611a1fcb
carlosmendez98 <carlosvalenciamendez@gmail.com>
2026-08-03 01:50:24 -0500

    Inicio de Front

--- src/main/java/com/example/proyectoAlura2026/demo/Controller/ContenidoController.java
+++ src/main/java/com/example/proyectoAlura2026/demo/Controller/ContenidoController.java
@@ -1,26 +1,46 @@
 package com.example.proyectoAlura2026.demo.Controller;
 
+
 import com.example.proyectoAlura2026.demo.Dto.RequestEnviar;
 import com.example.proyectoAlura2026.demo.Dto.ResponseContenido;
 import com.example.proyectoAlura2026.demo.Service.ConsultarMetadatos;
 import jakarta.validation.Valid; //Es la librería para el interruptor
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("api/contenido")
+@Controller
 public class ContenidoController {
 
     private final ConsultarMetadatos consultarMetadatos;
 
+
     public ContenidoController(ConsultarMetadatos consultarMetadatos) {
         this.consultarMetadatos = consultarMetadatos;
+
     }
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";   // Busca templates/index.html
+    }
+
+
     //
-    @PostMapping()
+    @PostMapping("contenido")
     //El Valid es el interruptor que enciende la seguridad.
-    public ResponseEntity<ResponseContenido> obtenerDatos(@Valid @RequestBody RequestEnviar request){
+    public String obtenerDatos(@Valid    @RequestParam String titulo,
+                                                          @RequestParam String texto, Model model){
+
+        RequestEnviar request  = new RequestEnviar(titulo, texto);
         ResponseContenido response =  this.consultarMetadatos.obtenerDatosResponse(request.titulo(), request.texto());
-        return ResponseEntity.ok(response);
+        model.addAttribute("titulo", titulo);
+        model.addAttribute("texto", texto);
+        model.addAttribute("categoria", response.getCategoria());
+        model.addAttribute("confianza", response.getConfianza());
+        return "index";
     }
+
+
+
 }
