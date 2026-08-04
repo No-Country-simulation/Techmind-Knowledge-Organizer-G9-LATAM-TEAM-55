9cf872fb1e6f0560286753d24aade7626501f045
Juan Luis Talavera Diaz <juantdtd@gmail.com>
2026-08-04 18:25:31 -0500

    Limitación de caracteres en el titulo y el cuerpo

--- src/main/java/com/example/proyectoAlura2026/demo/Controller/ContenidoController.java
+++ src/main/java/com/example/proyectoAlura2026/demo/Controller/ContenidoController.java
@@ -1,46 +1,23 @@
 package com.example.proyectoAlura2026.demo.Controller;
 
-
 import com.example.proyectoAlura2026.demo.Dto.RequestEnviar;
 import com.example.proyectoAlura2026.demo.Dto.ResponseContenido;
 import com.example.proyectoAlura2026.demo.Service.ConsultarMetadatos;
-import jakarta.validation.Valid; //Es la librería para el interruptor
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class ContenidoController {
 
     private final ConsultarMetadatos consultarMetadatos;
 
-
     public ContenidoController(ConsultarMetadatos consultarMetadatos) {
         this.consultarMetadatos = consultarMetadatos;
-
     }
 
-    @GetMapping("/")
-    public String inicio() {
-        return "index";   // Busca templates/index.html
+    @PostMapping("/contenido")
+    public ResponseContenido procesarContenido(@Valid @RequestBody RequestEnviar request) {
+        return this.consultarMetadatos.obtenerDatosResponse(request.titulo(), request.texto());
     }
-
-
-    //
-    @PostMapping("contenido")
-    //El Valid es el interruptor que enciende la seguridad.
-    public String obtenerDatos(@Valid    @RequestParam String titulo,
-                                                          @RequestParam String texto, Model model){
-
-        RequestEnviar request  = new RequestEnviar(titulo, texto);
-        ResponseContenido response =  this.consultarMetadatos.obtenerDatosResponse(request.titulo(), request.texto());
-        model.addAttribute("titulo", titulo);
-        model.addAttribute("texto", texto);
-        model.addAttribute("categoria", response.getCategoria());
-        model.addAttribute("confianza", response.getConfianza());
-        return "index";
-    }
-
-
-
 }
--- src/main/java/com/example/proyectoAlura2026/demo/Dto/RequestEnviar.java
+++ src/main/java/com/example/proyectoAlura2026/demo/Dto/RequestEnviar.java
@@ -1,12 +1,14 @@
 package com.example.proyectoAlura2026.demo.Dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RequestEnviar(
         @NotBlank(message = "El título es obligatorio y no puede estar vacío.")
+        @Size(max = 75, message = "El título no puede exceder los 75 caracteres.")
         String titulo,
 
         @NotBlank(message = "El texto es obligatorio y no puede estar vacío.")
+        @Size(max = 150, message = "El texto no puede exceder los 150 caracteres.")
         String texto
-) {
-}
+) {}
